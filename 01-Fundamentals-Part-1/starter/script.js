d2d0b6718b2db2071692b2ee6ed0e4a72bc91ab9
KonstiSmt <konsti@matchical.com>
2022-01-04 09:15:18 +0100

    fc

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+// const heightMark = 1.69;
+// const massMark = 78;
+//
+// const heightJohn = 1.95;
+// const massJohn = 92;
+//
+// // const heightMark = 1.88;
+// // const massMark = 95;
+// //
+// // const heightJohn = 1.76;
+// // const massJohn = 85;
+//
+// const bmiMark = massMark / heightMark ** 2;
+// const bmiJohn = massJohn / heightJohn ** 2;
+// const markHigherBmi = bmiMark > bmiJohn;
+//
+// console.log(bmiMark, bmiJohn, markHigherBmi);
+//
+// const name = 'Konsti';
+// const age = 27;
+// const greeting = `I'm ${name}, and ${age} years old.`
+//
+// console.log(greeting)
+//
+// if (markHigherBmi) {
+//     console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})`);
+// } else {
+//     console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})`)
+// }
+
