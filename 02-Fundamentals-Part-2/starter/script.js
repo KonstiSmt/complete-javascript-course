ab46f5fffe80a7dbfecf44ebc9732a28bc4f1653
KonstiSmt <konsti@matchical.com>
2022-01-07 07:49:40 +0100

    challenge 1

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,41 @@
+// const calcAge = function (birthYeah) {
+//   return 2037 - birthYeah;
+// }
+//
+// const yearsUntilRetirement = function (birthYeah, firstName) {
+//   const age = calcAge(birthYeah);
+//   const retirement = 65 - age;
+//
+//   // if (retirement > 0) {
+//   //   console.log(`${firstName} retires in ${retirement} years`);
+//   //   return retirement;
+//   // } else {
+//   //   console.log(`${firstName} has already retired 🎉`);
+//   //   return -1;
+//   // }
+//   return retirement > 0 ? retirement : -1;
+// }
+//
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+//
+// function checkWinner(avgDolphins, avgKoalas) {
+//     if (avgDolphins >= avgKoalas * 2) {
+//         console.log(`The Dolphins win the trophy 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas >= avgDolphins * 2) {
+//         console.log(`The Koalas win the trophy 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+//     } else {
+//         console.log(`Draw`);
+//     }
+// }
+//
+// let avgDolphins = calcAverage(44, 23, 71);
+// let avgKoalas = calcAverage(65, 54, 49);
+// checkWinner(avgDolphins, avgKoalas);
+//
+// avgDolphins = calcAverage(85, 54, 41);
+// avgKoalas = calcAverage(23, 34, 27);
+// checkWinner(avgDolphins, avgKoalas);
+
