909c39e9e3503b1d410a394653986a74bf1cc05d
KonstiSmt <konsti@matchical.com>
2022-01-13 07:54:10 +0100

    guess my number

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,13 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-function printForecast(arr) {
-    let forecasted = `...`;
-    for (let i = 0; i < arr.length; i++) {
-        forecasted += ` ${arr[i]}ºC in ${i + 1} days ...`;
-    }
-    console.log(forecasted);
-    return forecasted
-}
-
-console.log(printForecast([12, 5, -5, 0, 4]))
+// function printForecast(arr) {
+//     let forecasted = `...`;
+//     for (let i = 0; i < arr.length; i++) {
+//         forecasted += ` ${arr[i]}ºC in ${i + 1} days ...`;
+//     }
+//     console.log(forecasted);
+//     return forecasted
+// }
+//
+// console.log(printForecast([12, 5, -5, 0, 4]))
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+function setHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+    }
+}
+
+function reduceScore() {
+    score--;
+    if (!score) {
+        displayMessage(`💥 You lost the game!`);
+    } else {
+        document.querySelector('.score').textContent = score;
+    }
+}
+
+function displayMessage(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        displayMessage(`⛔️ No Number!`);
+    } else if (guess === secretNumber) {
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        displayMessage(`🎉 Correct Number!`);
+        setHighScore();
+    } else {
+        reduceScore();
+        displayMessage(guess > secretNumber ? `📈 Too high!` : `📉 Too low!`);
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = `?`;
+    document.querySelector('.score').textContent = score;
+    displayMessage(`Start guessing...`);
+    document.querySelector('.guess').value = "";
+    document.querySelector('body').style.backgroundColor = '#222';
+});
